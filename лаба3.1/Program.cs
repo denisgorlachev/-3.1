3775ea3f16f80c05fd3ca8036829c95885206b45
denisgorlachev <117777771+denisgorlachev@users.noreply.github.com>
2022-11-09 19:43:54 +0200

    Добавьте файлы проекта.

---
+++ лаба3.1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Горлачев базовый уровень 
+Console.WriteLine("Введите число N: ");
+double N = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число X: ");
+double X = double.Parse(Console.ReadLine());
+double sum = 0;
+for (int i = 0; i < N; i++)
+{
+    sum = -1 * N * X * 2 * N + 1 / (2 * N + 1);
+}
+Console.WriteLine("sum: " + sum);
